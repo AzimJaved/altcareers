1abbaa6db3e83e368d39212073fde14956d3e0e8
mauwazahmed <56031816+mauwazahmed@users.noreply.github.com>
2021-03-05 18:49:27 +0530

    Update predict.py

--- Alt_Career/predict.py
+++ Alt_Career/predict.py
@@ -95,40 +95,40 @@ def predict(ind,f_area,sk1,sk2,sk3,sk4,sk5,ind_,f_area_,sk1_,sk2_,sk3_,sk4_,sk5_
     # print(predicted_rolecat)
     if len(predicted_rolecat) > 0:
         for rol_cat in predicted_rolecat:
-            data2 = data[(data['Role Category'] == rol_cat) & (data['Industry'] != ind_)]
+            data2 = data[(data['Role Category'] == rol_cat)
             #display(data2)
-            ind_lt = list(set(data2['Industry'].values))
+            #ind_lt = list(set(data2['Industry'].values))
             role_lt = []
             d1 = []
-            for i in ind_lt:
-                intermed = []
-                intermed.append([rol_cat,i,3])
-                data3 = data2[data2['Industry'] == i]
-                role_lt = list(set(list(data3['Role'].values)))
-                for role in role_lt:
-                    intermed.append([i, role, 3])
-                for role in role_lt :
-                    data4 = data3[data3['Role'] == role]
-                    sc = ['Skill1','Skill2','Skill3','Skill4','Skill5']
-                    sk_dct = {}
-                    for c in sc:
-                        for skill in list(data4[c].values):
-                            sk_dct[skill] = sk_dct.get(skill, 0) + 1
-                    count = list(sk_dct.values())
-                    sort_dct = sorted(sk_dct.items(), key = lambda ele: ele[1], reverse = True)
-                    sort_skill = [x[0] for x in sort_dct]
-                    #print(sort_dct)
-                    if max(count) < 5:
-                        b = sort_skill[0:30]
-                    else:
-                        b = sort_skill[0:20]
-                    d = [i,role] + readiness(b, sk_inp)
-                    d1.append(d)
-                    top_skill = b[0:5]
-                    for sk in top_skill:
-                        unique = " " + str(sk) + " "
-                        intermed.append([role, unique, 3])
-                final.append(intermed)
+            #for i in ind_lt:
+            intermed = []
+            #intermed.append([rol_cat,i,3])
+            #data3 = data2[data2['Industry'] == i]
+            role_lt = list(set(list(data2['Role'].values)))
+            for role in role_lt:
+                intermed.append([rol_cat, role, 4])
+            for role in role_lt :
+                data4 = data2[data2['Role'] == role]
+                sc = ['Skill1','Skill2','Skill3','Skill4','Skill5']
+                sk_dct = {}
+                for c in sc:
+                    for skill in list(data4[c].values):
+                        sk_dct[skill] = sk_dct.get(skill, 0) + 1
+                count = list(sk_dct.values())
+                sort_dct = sorted(sk_dct.items(), key = lambda ele: ele[1], reverse = True)
+                sort_skill = [x[0] for x in sort_dct]
+                #print(sort_dct)
+                if max(count) < 5:
+                    b = sort_skill[0:30]
+                else:
+                    b = sort_skill[0:20]
+                d = [role] + readiness(b, sk_inp)
+                d1.append(d)
+                top_skill = b[0:5]
+                for sk in top_skill:
+                    unique = " " + str(sk) + " "
+                    intermed.append([role, unique, 3])
+            final.append(intermed)
             ready.append(d1)
 
     '''
