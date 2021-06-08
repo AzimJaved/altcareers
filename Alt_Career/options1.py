0559b87129e4909a4458875697140bd17abf4635
mauwazahmed <56031816+mauwazahmed@users.noreply.github.com>
2021-06-08 22:22:50 +0530

    Update options1.py

--- Alt_Career/options1.py
+++ Alt_Career/options1.py
@@ -1,80 +1,90 @@
 import pandas as pd
-import numpy as np
 import json
 import os
 from rest_framework import status
 from django.http import JsonResponse
 from .settings import BASE_URL, BASE_DIR
-import csv
 
-with open(BASE_DIR + '/Alt_Career/csv/option.json','r') as f:
-    skills_dct = json.load(f)
+
+
 
 def skill0_info():
+    with open(BASE_DIR + '/Alt_Career/csv/skills.json','r') as f:
+        skills_dct = json.load(f)
     data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
-    m_enc = list(set(data_enc['Skill1'].values))
+    #m_enc = list(set(data_enc['Skill1'].values))
+    #m_enc = list(set(skills_dct.keys()))
     # code = options_dct[s1]
     # if code in m_enc:
     #     m_enc.remove(code)
-    info_dct = [key for key,val in skills_dct.items()]
+    info_dct = list(set(skills_dct.keys()))
     return JsonResponse({"data": info_dct}, status=status.HTTP_200_OK)
 
 def skill1_info(s1):
-    data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
-    m_enc = list(set(data_enc['Skill2'].values))
-    code = skills_dct[s1]
-    if code in m_enc:
-        m_enc.remove(code)
-    info_dct = [key for key,val in skills_dct.items() if val in m_enc]
+    with open(BASE_DIR + '/Alt_Career/csv/skills.json','r') as f:
+        skills_dct = json.load(f)
+    #data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
+    #m_enc = list(set(data_enc['Skill2'].values))
+    #m_enc = list(set(skills_dct.values()))
+    #code = skills_dct[s1]
+    #if code in m_enc:
+    #    m_enc.remove(code)
+    del skills_dct[s1]
+    info_dct = list(set(skills_dct.keys()))
     return JsonResponse({"data": info_dct}, status=status.HTTP_200_OK)
 
 
 def skill2_info(s1, s2):
-    data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
-    m_enc = list(set(data_enc['Skill3'].values))
-    code = [skills_dct[s1], skills_dct[s2]]
-    for c in code:
-        if c in m_enc:
-            m_enc.remove(c)
-    info_dct = [key for key,val in skills_dct.items() if val in m_enc]
+    with open(BASE_DIR + '/Alt_Career/csv/skills.json','r') as f:
+        skills_dct = json.load(f)
+    #data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
+    #m_enc = list(set(data_enc['Skill3'].values))
+    code = [s1, s2]
+    for s in code:
+        del skills_dct[s]
+    info_dct = list(set(skills_dct.keys()))
     return JsonResponse({"data": info_dct}, status=status.HTTP_200_OK)
 
 def skill3_info(s1, s2, s3):
-    data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
-    m_enc = list(set(data_enc['Skill4'].values))
-    code = [skills_dct[s1], skills_dct[s2], skills_dct[s3]]
-    for c in code:
-        if c in m_enc:
-            m_enc.remove(c)
-    info_dct = [key for key,val in skills_dct.items() if val in m_enc]
+    with open(BASE_DIR + '/Alt_Career/csv/skills.json','r') as f:
+        skills_dct = json.load(f)
+    #data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
+    #m_enc = list(set(data_enc['Skill3'].values))
+    code = [s1, s2, s3]
+    for s in code:
+        del skills_dct[s]
+    info_dct = list(set(skills_dct.keys()))
     return JsonResponse({"data": info_dct}, status=status.HTTP_200_OK)
 
 def skill4_info(s1, s2, s3, s4):
-    data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
-    m_enc = list(set(data_enc['Skill5'].values))
-    code = [skills_dct[s1], skills_dct[s2], skills_dct[s3], skills_dct[s4]]
-    for c in code:
-        if c in m_enc:
-            m_enc.remove(c)
-    info_dct = [key for key,val in skills_dct.items() if val in m_enc]
+    with open(BASE_DIR + '/Alt_Career/csv/skills.json','r') as f:
+        skills_dct = json.load(f)
+    #data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
+    #m_enc = list(set(data_enc['Skill3'].values))
+    code = [s1, s2, s3, s4]
+    for s in code:
+        del skills_dct[s]
+    info_dct = list(set(skills_dct.keys()))
     return JsonResponse({"data": info_dct}, status=status.HTTP_200_OK)
 
 def skill5_info(s1, s2, s3, s4, s5):
-    data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
-    m_enc = list(set(data_enc['Skill6'].values))
-    code = [skills_dct[s1], skills_dct[s2], skills_dct[s3], skills_dct[s4], skills_dct[s5]]
-    for c in code:
-        if c in m_enc:
-            m_enc.remove(c)
-    info_dct = [key for key,val in skills_dct.items() if val in m_enc]
+    with open(BASE_DIR + '/Alt_Career/csv/skills.json','r') as f:
+        skills_dct = json.load(f)
+    #data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
+    #m_enc = list(set(data_enc['Skill3'].values))
+    code = [s1, s2, s3, s4, s5]
+    for s in code:
+        del skills_dct[s]
+    info_dct = list(set(skills_dct.keys()))
     return JsonResponse({"data": info_dct}, status=status.HTTP_200_OK)
 
 def skill6_info(s1, s2, s3, s4, s5, s6):
-    data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
-    m_enc = list(set(data_enc['Skill7'].values))
-    code = [skills_dct[s1], skills_dct[s2], skills_dct[s3], skills_dct[s4], skills_dct[s5], skills_dct[s6]]
-    for c in code:
-        if c in m_enc:
-            m_enc.remove(c)
-    info_dct = [key for key,val in skills_dct.items() if val in m_enc]
+    with open(BASE_DIR + '/Alt_Career/csv/skills.json','r') as f:
+        skills_dct = json.load(f)
+    #data_enc = pd.read_csv(BASE_DIR + "/Alt_Career/csv/job_dataset_encoded.csv")
+    #m_enc = list(set(data_enc['Skill3'].values))
+    code = [s1, s2, s3, s4, s5, s6]
+    for s in code:
+        del skills_dct[s]
+    info_dct = list(set(skills_dct.keys()))
     return JsonResponse({"data": info_dct}, status=status.HTTP_200_OK)
